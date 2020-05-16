891191f2b7b1f749af6ee415ec8a79f60fc5c326
Matteo Malvestiti <matteo.malvestiti@gmail.com>
2020-05-16 15:12:47 +0200

    other mods

---
+++ TowerDefense2/Assets/Script/EnemySpawnerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+//indico cosa deve generare
+public GameObject enemyPrefab;
+    public float respawnTime = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyWawe());
+    }
+
+    //nuova funzione che si occupa dello spawn
+
+    private void SpawnEnemy()
+    {
+        //crea un nuovo GameObject basato su enemyPrefab
+        GameObject a = Instantiate(enemyPrefab) as GameObject;
+    }
+
+    IEnumerator EnemyWawe()
+    {
+
+        while (true)
+        {
+            print(Time.time);
+            //Al passare del tempo lo spawn diventa sempre più veloce
+             yield return new WaitForSeconds(respawnTime - (Time.time * 0.01f));
+
+            SpawnEnemy();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+}
